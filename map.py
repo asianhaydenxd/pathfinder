7bb8cec980c7e2b317ad7abf55d65b98b3389b51
Hayden <asianhaydenxd@gmail.com>
2021-07-22 09:54:57 -0400

    Add pathfinding algorithm

--- map.py
+++ map.py
@@ -1,3 +1,5 @@
+from node import Node
+
 class Map:
     def __init__(self, map):
         self.map = map
@@ -14,3 +16,43 @@ class Map:
             if target in row:
                 return Node(index, row.index(target), None)
         raise ValueError('No value exists in map')
+
+    def get_best_node(self, node_list):
+        hashmap = []
+        best_node = node_list[0]
+        
+        for node in node_list:
+            node_total_distance = node.get_total_distance(self.start, self.target)
+            hashmap.append(node_total_distance)
+            if min(hashmap) == node_total_distance:
+                best_node = node
+        
+        return best_node
+    
+    def get_end_of_path(self):
+        open = []
+        closed = []
+        open.append(self.start)
+
+        while True:
+            current = self.get_best_node(open)
+            open.remove(current)
+            closed.append(current)
+
+            if current.row == self.target.row and current.col == self.target.col:
+                print(f'{len(open)+len(closed)} checks')
+                return current
+            
+            for neighbor in current.get_neighbors():
+                if neighbor.is_blocked(self) or neighbor.is_in_list(closed):
+                    continue
+
+                if not neighbor in open:
+                    open.append(neighbor)
+            
+            print(f'{current.get_total_distance(self.start, self.target)} {current.get_coords()}')
+    
+    def get_path(self):
+        return self.get_end_of_path().get_path_as_list()
+    
+    
--- node.py
+++ node.py
@@ -9,3 +9,67 @@ class Node:
     
     def get_coords(self):
         return (self.row, self.col)
+    
+    def get_total_distance(self, start, target):
+        return self.get_distance_from_start(start) + self.get_distance_from_target(target)
+    
+    def get_distance_from_start(self, start):
+        return self.get_horizontal_distance(start) + self.get_vertical_distance(start)
+    
+    def get_distance_from_target(self, target):
+        return (self.get_horizontal_distance(target) + self.get_vertical_distance(target))*3
+    
+    def get_horizontal_distance(self, node):
+        return abs(self.row - node.row)
+    
+    def get_vertical_distance(self, node):
+        return abs(self.col - node.col)
+    
+    def get_neighbors(self):
+        neighbor_north = Node(self.row - 1, self.col, self)
+        neighbor_south = Node(self.row + 1, self.col, self)
+
+        neighbor_west = Node(self.row, self.col - 1, self)
+        neighbor_east = Node(self.row, self.col + 1, self)
+
+        neighbors = [neighbor_north, neighbor_east, neighbor_south, neighbor_west]
+        
+        return neighbors
+    
+    def is_blocked(self, map):
+        if self.is_outside_lower_bound():
+            return True
+        if self.is_outside_upper_bound(map):
+            return True
+        if self.is_wall(map):
+            return True
+        return False
+    
+    def is_outside_lower_bound(self):
+        return self.row < 0 or self.col < 0
+    
+    def is_outside_upper_bound(self, map):
+        return self.row + 1 > map.height or self.col + 1 > map.width
+    
+    def is_wall(self, map):
+        return map.map[self.row][self.col] == 1
+    
+    def is_in_list(self, list):
+        for node in list:
+            if self.is_equal_to(node):
+                return True
+        return False
+    
+    def is_equal_to(self, node):
+        return node.row == self.row and node.col == self.col
+
+    def get_path_as_list(self):
+        node = self
+        path_list = [(node.row, node.col)]
+
+        while node.parent != None:
+            node = node.parent
+            path_list.append((node.row, node.col))
+
+        path_list.reverse()
+        return path_list
