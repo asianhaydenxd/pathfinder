05229c49b4ec31b48ad6b51f86b0041abd9538b6
Hayden <asianhaydenxd@gmail.com>
2021-07-23 14:58:47 -0400

    Update .gitignore

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-key1 = 'key1'
-key2 = 'key1'
+from map import MapCompiler as mapc
 
-keys = {
-    key1: 1,
-    key2: 2,
-}
-
-print(len(keys) == len(set(keys)))
+compiled = mapc([
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [2, 0, 1, 0, 1],
+]).compile()
