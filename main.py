41f61f3d2227828b29d65b98d4b20d0622dee5eb
Hayden <asianhaydenxd@gmail.com>
2021-07-22 23:16:30 -0400

    Reformat main.py for user reference

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from map import Map
-from node import Node
+from node import Node, NodeState
 from plotmap import PlotMap
 
-map = Map([
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 3],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 1],
-])
-
-map2 = Map([
+small_map = Map([
     [0, 0, 0, 0, 0],
     [0, 0, 1, 0, 3],
     [0, 0, 1, 0, 0],
@@ -18,23 +10,27 @@ map2 = Map([
     [2, 0, 1, 0, 1],
 ])
 
-map3 = Map([
+large_map = Map([
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,1,3,0],
+    [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+    [0,1,1,1,1,0,0,0,1,1,1,0,0,0,0],
+    [0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,2,0,0,0,1,0,0,0,1,0,0,0,0,0],
+    [0,0,1,0,0,1,0,0,1,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
-    [0,2,0,0,0,1,0,0,0,1,3,0,0,0,0],
-    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ])
 
-path = map3.get_path()
+custom_map = Map.new(100, 100, (0, 0), (99, 99))
+
+pltmap = PlotMap(custom_map)
+pltmap.plot()
+
+path = custom_map.get_path()
 print(f'Node: {path.node}')
 print(f'Open: {Node.list_coords(path.open)}')
 print(f'Closed: {Node.list_coords(path.closed)}')
 print(f'Checks: {path.checks}')
 print(f'List: {Node.list_coords(path.list)}')
-
-pltmap = PlotMap(map3)
-pltmap.plot()
