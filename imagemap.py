300c6f446093cbf7abe24fc10aa8ee1051ea3f65
Hayden <asianhaydenxd@gmail.com>
2021-07-25 00:35:13 -0400

    Make ImageMap colormap editable by the user

--- imagemap.py
+++ imagemap.py
@@ -8,16 +8,16 @@ class ImageMap:
         self.image_name = image_name
         self.image = Image.open(image_name).convert('RGB')
         self.matrix = numpy.array(self.image)
-    
-    def generate_map(self):
-        return [[self.get_node_type(node) for node in row] for row in self.matrix]
-    
-    def get_node_type(self, node):
-        colormap = {
+        self.colormap = {
             (255, 255, 255): NodeType.BLANK.value,
             (0, 0, 0): NodeType.WALL.value,
             (0, 255, 0): NodeType.START.value,
             (255, 0, 0): NodeType.TARGET.value,
         }
+    
+    def generate_map(self):
+        return [[self.get_node_type(node) for node in row] for row in self.matrix]
+    
+    def get_node_type(self, node):
         node = tuple(node)
-        return colormap[node]
+        return self.colormap[node]
