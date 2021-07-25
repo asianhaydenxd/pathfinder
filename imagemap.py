81a69fc78416ba4917f8775a03f4a9f3d94e6282
Hayden <asianhaydenxd@gmail.com>
2021-07-25 00:32:09 -0400

    Have ImageMap require exact colors
    Requiring exact colors instead of allowing a range of different nearby colors improves performance speed drastically.

--- imagemap.py
+++ imagemap.py
@@ -14,20 +14,10 @@ class ImageMap:
     
     def get_node_type(self, node):
         colormap = {
-            'white': NodeType.BLANK.value,
-            'black': NodeType.WALL.value,
-            'lime': NodeType.START.value,
-            'red': NodeType.TARGET.value,
+            (255, 255, 255): NodeType.BLANK.value,
+            (0, 0, 0): NodeType.WALL.value,
+            (0, 255, 0): NodeType.START.value,
+            (255, 0, 0): NodeType.TARGET.value,
         }
-        return colormap[self.closest_color(node)]
-    
-    def closest_color(self, requested_color):
-        requested_color = tuple(requested_color)
-        min_colors = {}
-        for key, name in webcolors.html4_hex_to_names.items():
-            r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-            rd = (r_c - requested_color[0]) ** 2
-            gd = (g_c - requested_color[1]) ** 2
-            bd = (b_c - requested_color[2]) ** 2
-            min_colors[(rd + gd + bd)] = name
-        return min_colors[min(min_colors.keys())]
+        node = tuple(node)
+        return colormap[node]
